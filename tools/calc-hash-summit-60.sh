95419296d4c9b60f9f1d7abaff2b5e52ee191039
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2021-12-07 11:03:04 -0700

    meta-laird-cp: Added support for utilizing mirrors
    Bug 20579

---
+++ tools/calc-hash-summit-60.sh
@@ -0,0 +1,32 @@
+[ -z "${1}" ] && { echo "Usage: $0 <version>"; exit 1; }
+
+file="radio-stack-summit-60-hashes.inc"
+prefix="https://files.devops.rfpros.com/builds/linux"
+ver=${1}
+
+calc_file () {
+  name=${1##*/}
+  wget ${1} || exit 1
+  echo "SRC_URI[${2}.md5sum] = \"$(md5sum ${name} | awk '{print $1}')\"" >> ${file}
+  echo "SRC_URI[${2}.sha256sum] = \"$(sha256sum ${name} | awk '{print $1}')\"" >> ${file}
+  rm -f ${name}
+}
+
+echo -e "PV = \"${ver}\"\n" > ${file}
+
+for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
+do
+  calc_file "${prefix}/summit_supplicant/laird/${ver}/summit_supplicant-${i}-${ver}.tar.bz2" "summit-supplicant-${i}"
+  calc_file "${prefix}/summit_supplicant/laird/${ver}/summit_supplicant_openssl_1_0_2-${i}-${ver}.tar.bz2" "summit-supplicant_openssl_1_0_2-${i}"
+  calc_file "${prefix}/adaptive_ww/laird/${ver}/adaptive_ww-${i}-${ver}.tar.bz2" "adaptive-ww-${i}"
+  calc_file "${prefix}/adaptive_ww/laird/${ver}/adaptive_ww_openssl_1_0_2-${i}-${ver}.tar.bz2" "adaptive-ww_openssl_1_0_2-${i}"
+done
+
+calc_file "${prefix}/adaptive_bt/src/${ver}/adaptive_bt-src-${ver}.tar.gz" "adaptive-bt"
+calc_file "${prefix}/lrd-network-manager/src/${ver}/lrd-network-manager-src-${ver}.tar.xz" "lrd-network-manager"
+calc_file "${prefix}/backports/laird/${ver}/backports-laird-${ver}.tar.bz2" "backports-laird"
+
+for i in pcie-uart pcie-usb sdio-uart sdio-sdio usb-usb usb-uart
+do
+  calc_file "${prefix}/firmware/${ver}/laird-summit60-firmware-${i}-${ver}.tar.bz2" "summit60-firmware-${i}"
+done
