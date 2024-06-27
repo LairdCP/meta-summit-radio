e0d0a19df915cf812a2b1664d2169d2d29ebe7ac
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-06-27 17:55:23 -0400

    Add fileshare auth support and other improvements

--- tools/calc-hash-60.sh
+++ tools/calc-hash-60.sh
@@ -4,18 +4,21 @@
 
 ver=${1}
 
+[ -z "${RFPROS_FILESHARE_USER}" ] || \
+  RFPROS_FILESHARE_AUTH="${RFPROS_FILESHARE_USER}:${RFPROS_FILESHARE_PASS}@"
+
 file="radio-stack-60-hashes.inc"
-prefix="https://files.devops.rfpros.com/builds/linux"
+prefix="https://${RFPROS_FILESHARE_AUTH}files.devops.rfpros.com/builds/linux"
 prefixext="https://github.com/LairdCP/Sterling-60-Release-Packages/releases/download/LRD-REL-${ver}"
 
 calc_file () {
-  wget ${prefix}/${1}/${ver}/${2} || wget ${prefixext}/${2} || exit 1
-  echo "SRC_URI[${3}.md5sum] = \"$(md5sum ${2} | awk '{print $1}')\"" >> ${file}
-  echo "SRC_URI[${3}.sha256sum] = \"$(sha256sum ${2} | awk '{print $1}')\"" >> ${file}
-  rm -f ${2}
+  wget -t 1 -T 4 "${prefix}/${1}/${ver}/${2}" || wget -t 1 -T 4 "${prefixext}/${2}" || exit 1
+  echo "SRC_URI[${3}.md5sum] = \"$(md5sum "${2}" | awk '{print $1}')\"" >> ${file}
+  echo "SRC_URI[${3}.sha256sum] = \"$(sha256sum "${2}" | awk '{print $1}')\"" >> ${file}
+  rm -f "${2}"
 }
 
-echo "RADIO_VERSION = \"${ver}\"\n" > ${file}
+printf 'RADIO_VERSION = "%s"\n' "${ver}" > ${file}
 
 for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
 do
