609393ebc7db8a18eda51306a3f73194b6b74b34
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-06-28 03:26:11 -0400

    Improved file share support

--- tools/calc-hash-60.sh
+++ tools/calc-hash-60.sh
@@ -1,24 +1,13 @@
 #! /bin/sh
 
-[ -z "${1}" ] && { echo "Usage: $0 <version>"; exit 1; }
-
+dir=$(dirname "${0}")
 ver=${1}
 
-[ -z "${RFPROS_FILESHARE_USER}" ] || \
-  RFPROS_FILESHARE_AUTH="${RFPROS_FILESHARE_USER}:${RFPROS_FILESHARE_PASS}@"
-
+# shellcheck disable=SC2034
 file="radio-stack-60-hashes.inc"
-prefix="https://${RFPROS_FILESHARE_AUTH}files.devops.rfpros.com/builds/linux"
-prefixext="https://github.com/LairdCP/Sterling-60-Release-Packages/releases/download/LRD-REL-${ver}"
 
-calc_file () {
-  wget -t 1 -T 4 "${prefix}/${1}/${ver}/${2}" || wget -t 1 -T 4 "${prefixext}/${2}" || exit 1
-  echo "SRC_URI[${3}.md5sum] = \"$(md5sum "${2}" | awk '{print $1}')\"" >> ${file}
-  echo "SRC_URI[${3}.sha256sum] = \"$(sha256sum "${2}" | awk '{print $1}')\"" >> ${file}
-  rm -f "${2}"
-}
-
-printf 'RADIO_VERSION = "%s"\n' "${ver}" > ${file}
+# shellcheck source=/dev/null
+. "${dir}/calc-common.sh"
 
 for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
 do
--- tools/calc-hash-msd.sh
+++ tools/calc-hash-msd.sh
@@ -1,23 +1,13 @@
 #! /bin/sh
 
-[ -z "${1}" ] && { echo "Usage: $0 <version>"; exit 1; }
-
+dir=$(dirname "${0}")
 ver=${1}
 
-[ -z "${RFPROS_FILESHARE_USER}" ] || \
-  RFPROS_FILESHARE_AUTH="${RFPROS_FILESHARE_USER}:${RFPROS_FILESHARE_PASS}@"
-
+# shellcheck disable=SC2034
 file="radio-stack-msd-hashes.inc"
-prefix="https://${RFPROS_FILESHARE_AUTH}files.devops.rfpros.com/builds/linux"
 
-calc_file () {
-  wget -t 1 -T 4 "${prefix}/${1}/${ver}/${2}" || exit 1
-  echo "SRC_URI[${3}.md5sum] = \"$(md5sum "${2}" | awk '{print $1}')\"" >> ${file}
-  echo "SRC_URI[${3}.sha256sum] = \"$(sha256sum "${2}" | awk '{print $1}')\"" >> ${file}
-  rm -f "${2}"
-}
-
-printf 'RADIO_VERSION = "%s"\n' "${ver}" > ${file}
+# shellcheck source=/dev/null
+. "${dir}/calc-common.sh"
 
 for i in arm-eabi arm-eabihf
 do
