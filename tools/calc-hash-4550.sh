8d58aa41c7009afb0970d4e0346f0e3008b1a90b
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-09-06 18:00:49 -0400

    Added fileshare authenticatin support to hashh calculation scripts

--- tools/calc-hash-4550.sh
+++ tools/calc-hash-4550.sh
@@ -2,12 +2,16 @@
 
 ver=${1}
 
+wget="/usr/bin/wget -T4 -t1"
+
+[ -z "${RFPROS_FILESHARE_USER}" ] || \
+	wget="${wget} --user=${RFPROS_FILESHARE_USER} --password=${RFPROS_FILESHARE_PASS} --auth-no-challenge"
+
 file="radio-stack-4550-hashes.inc"
 prefix="https://files.devops.rfpros.com/builds/linux"
 
 calc_file () {
-  name=${1##*/}
-  wget ${prefix}/${1}/${ver}/${2} || exit 1
+  ${wget} "${prefix}/${1}/${ver}/${2}" || exit 1
   echo "SRC_URI[${3}.md5sum] = \"$(md5sum ${2} | awk '{print $1}')\"" >> ${file}
   echo "SRC_URI[${3}.sha256sum] = \"$(sha256sum ${2} | awk '{print $1}')\"" >> ${file}
   rm -f ${2}
